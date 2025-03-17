96d1478fdb417f4e521d1a938644c3ab6399a2ba
SlangTang <93gaget@gmail.com>
2025-03-17 10:51:24 -0500

    Search results page implemented

---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = (_props:any) => {
+    return (
+      <section>
+        <h1>404: Page Not Found</h1>
+        <h1> ¯\_(ツ)_/¯</h1>
+      </section>
+    );
+  };
+  
+  export default ErrorPage;
