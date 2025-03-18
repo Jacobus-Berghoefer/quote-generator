1296c18f72c7246300c5cb614f2426f8af505727
SlangTang <93gaget@gmail.com>
2025-03-18 10:10:20 -0500

    styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,13 +8,13 @@ import { useNavigate } from "react-router";
 const Home = (props:any) => {
     let navigate = useNavigate();
     const [randomQuote, setRandomQuote] = useState<Quote>({
-        "q": "",
-        "a": ""
+        text: "",
+        author: ""
     })
 
     const [dailyQuote, setDailyQuote] = useState<Quote>({
-        "q": "",
-        "a": ''
+        text: "",
+        author: ''
     })
     const [_error, setError] = useState(false);
 
@@ -60,15 +60,22 @@ const Home = (props:any) => {
 
     return (
       <>
-        <section className="flicker">
+        <section className="daily-quote">
+            <div>
+                <p>{dailyQuote.text}</p>
+                <p>{dailyQuote.author}</p>
+            </div>
+        </section>
+
+        {/* <section className="flicker">
           <div className="el">
-            {/* <blockquote>{dailyQuote.q}</blockquote>
-                <p>{dailyQuote.a}</p> */}
+            <blockquote>{dailyQuote.q}</blockquote>
+                <p>{dailyQuote.a}</p>
           </div>
 
         </section>
 
-        {/* <div className="infinity">
+        <div className="infinity">
                 <blockquote>{randomQuote.q}</blockquote>
                 <p>{dailyQuote.a}</p>
                 <button className="random-quote-button" onClick={() => fetchRandomQuote()}>Get Another</button>
@@ -91,14 +98,34 @@ const Home = (props:any) => {
         </section>
 
         <section className="category-btns">
+            <button onClick={() => {navigate("search/?q=Anxiety")}}>Anxiety</button>
+            <button onClick={() => {navigate("search/?q=Change")}}>Change</button>
+            <button onClick={() => {navigate("search/?q=Choice")}}>Choice</button>
+            <button onClick={() => {navigate("search/?q=Confidence")}}>Confidence</button>
+            <button onClick={() => {navigate("search/?q=Courage")}}>Courage</button>
+            <button onClick={() => {navigate("search/?q=Death")}}>Death</button>
+            <button onClick={() => {navigate("search/?q=Dreams")}}>Dreams</button>
+            <button onClick={() => {navigate("search/?q=Excellence")}}>Excellence</button>
+            <button onClick={() => {navigate("search/?q=Failure")}}>Failure</button>
+            <button onClick={() => {navigate("search/?q=Fairness")}}>Fairness</button>
+            <button onClick={() => {navigate("search/?q=Fear")}}>Fear</button>
+            <button onClick={() => {navigate("search/?q=Forgiveness")}}>Forgiveness</button>
+            <button onClick={() => {navigate("search/?q=Freedom")}}>Freedom</button>
             <button onClick={() => {navigate("search/?q=Future")}}>Future</button>
-            <button>Health</button>
-            <button>Love</button>
-            <button>Work</button>
-            <button>Success</button>
-            <button>Wisdom</button>
-            <button>Friendship</button>
-            <button>Money</button>
+            <button onClick={() => {navigate("search/?q=Happiness")}}>Happiness</button>
+            <button onClick={() => {navigate("search/?q=Inspiration")}}>Inspiration</button>
+            <button onClick={() => {navigate("search/?q=Kindness")}}>Kindness</button>
+            <button onClick={() => {navigate("search/?q=Leadership")}}>Leadership</button>
+            <button onClick={() => {navigate("search/?q=Life")}}>Life</button>
+            <button onClick={() => {navigate("search/?q=Living")}}>Living</button>
+            <button onClick={() => {navigate("search/?q=Love")}}>Love</button>
+            <button onClick={() => {navigate("search/?q=Pain")}}>Pain</button>
+            <button onClick={() => {navigate("search/?q=Past")}}>Past</button>
+            <button onClick={() => {navigate("search/?q=Success")}}>Success</button>
+            <button onClick={() => {navigate("search/?q=Time")}}>Time</button>
+            <button onClick={() => {navigate("search/?q=Today")}}>Today</button>
+            <button onClick={() => {navigate("search/?q=Truth")}}>Truth</button>
+            <button onClick={() => {navigate("search/?q=Work")}}>Work</button>
         </section>
 
         {/* <h1>{randomRecipe.title}</h1>
