e5efeb9494008786b869f4c0f95b84fe6442fffa
B A <blakeroband@gmail.com>
2025-03-19 12:57:09 -0500

    removed some logs

--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -2,7 +2,7 @@ import { typeDefs } from './typeDefs.js';
 import { resolvers } from './resolvers.js';
 
 console.log("📌 Checking Schema & Resolvers");
-console.log("🔍 TypeDefs:", typeDefs);
+// console.log("🔍 TypeDefs:", typeDefs);
 console.log("🔍 Resolvers:", JSON.stringify(resolvers, null, 2));
 
 export { typeDefs, resolvers };
